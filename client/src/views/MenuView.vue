fe33f117b000a0bb4f954a4209ce01234f98b569
Marcus Martinez <marcus224m@gmail.com>
2023-07-19 18:03:43 -0400

    added route to checkout button

--- client/src/views/MenuView.vue
+++ client/src/views/MenuView.vue
@@ -31,13 +31,13 @@ onMounted(() => {
     </div>
     <!---->
    <div class="flex items-center justify-center gap-4 fixed bottom-1 w-60">
-    <a href="#_" class="relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out border-2 bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl rounded-full shadow-md group">
+    <router-link to="/cart" class="relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out border-2 bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl rounded-full shadow-md group">
 <span class="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl group-hover:translate-x-0 ease">
 <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
 </span>
 <span class="absolute flex items-center justify-center w-full h-full text-white transition-all duration-300 transform group-hover:translate-x-full ease">Checkout</span>
 <span class="relative invisible">Checkout</span>
-</a>
+</router-link>
    </div>
 </div>
     <div class="flex items-center justify-center py-4 md:py-8 flex-wrap bg-orange-300">
